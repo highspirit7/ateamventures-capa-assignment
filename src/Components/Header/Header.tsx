dd89cd8d819a678aa8a2140b71c1e32704023c9c
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2021-09-11 17:05:49 +0900

    Add : 햄버거 메뉴 추가 및 반응형 breakpoints 위한 패키지 설치

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { up, down, between, only } from "styled-breakpoints";
 
 import capaPartnersLogo from "assets/images/capa-partners-logo.png";
 import companyIcon from "assets/icons/company.png";
@@ -8,15 +9,24 @@ import divider from "assets/icons/divider.png";
 const Header: React.FC = () => {
   return (
     <StyledHeader>
+      <HamburgerMenu>
+        <span></span>
+        <span></span>
+        <span></span>
+      </HamburgerMenu>
       <StyledLogo>
         <img src={capaPartnersLogo} alt="logo" />
       </StyledLogo>
       <StyledRightMenu>
-        <button>
-          <img src={companyIcon} alt="company_icon" />A 가공 업체
-        </button>
+        <li>
+          <a href="#">
+            <img src={companyIcon} alt="company_icon" />A 가공 업체
+          </a>
+        </li>
         <img src={divider} alt="divider" />
-        <button>로그아웃</button>
+        <li>
+          <a href="#">로그아웃</a>
+        </li>
       </StyledRightMenu>
     </StyledHeader>
   );
@@ -37,20 +47,40 @@ const StyledHeader = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
+
+  ${only("xs")} {
+    justify-content: flex-start;
+    padding: 0 23px;
+  }
 `;
 
-const StyledLogo = styled.div``;
+const StyledLogo = styled.div`
+  img {
+    ${only("xs")} {
+      width: 92px;
+      height: 12px;
+    }
+  }
+`;
 
-const StyledRightMenu = styled.div`
+const StyledRightMenu = styled.ul`
   width: 208px;
   display: flex;
   justify-content: space-between;
 
-  button {
+  ${only("xs")} {
+    display: none;
+  }
+
+  li {
     display: flex;
     align-items: center;
-    color: white;
-    font-size: 14px;
+    a {
+      display: flex;
+      align-items: center;
+      color: white;
+      font-size: 14px;
+    }
 
     img {
       margin-right: 8px;
@@ -58,3 +88,25 @@ const StyledRightMenu = styled.div`
     }
   }
 `;
+
+const HamburgerMenu = styled.div`
+  display: none;
+  margin-right: 20px;
+
+  ${only("xs")} {
+    display: block;
+    cursor: pointer;
+  }
+
+  span {
+    display: block;
+    width: 18px;
+    height: 2px;
+    margin: 3px auto;
+    -webkit-transition: all 0.3s ease-in-out;
+    transition: all 0.3s ease-in-out;
+    background-color: white;
+  }
+`;
+
+const StyledDrawer = styled.div``;
