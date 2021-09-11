2c1c3fc318ac3a209a64b2787f0e6365b4877ff5
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2021-09-11 12:01:32 +0900

    Init : 프로젝트 시작을 위한 세팅
    - eslint, prettier 세팅 with TypeScript
    - styled-components 세팅 : theme 및 GlobalStyles 적용
    - 불필요 파일 삭제
    - typescript template으로 CRA 설치
    - 카파 실 서비스에 맞추어 index.html, 파비콘 등 수정

---
+++ src/Pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import styled from "styled-components";
+
+const Home: React.FC = () => {
+  return <div>HOME</div>;
+};
+export default Home;
+
+// const Main = styled.main`
+//   min-height: 100vh;
+//   padding-top: 120px;
+// `;
---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+import { DefaultTheme } from "styled-components";
+
+const theme: DefaultTheme = {
+  colors: {
+    primary: "#1565C0",
+    second: "#2196F3",
+  },
+};
+
+export { theme };
